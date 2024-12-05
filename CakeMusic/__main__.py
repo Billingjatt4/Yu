f1e4e5455f02af4626a006c555a8f9e25e6c8795
Teamonix <unioxo@outlook.com>
2024-12-06 02:42:59 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -1,15 +1,15 @@
-import asyncio, logging, sys, os
-from pyrogram import idle  
+import asyncio
+from pyrogram import idle
 from CakeMusic import bot, app, call
-from CakeMusic.plugins import ALL_MODULES
 
 loop = asyncio.get_event_loop()
 
-LOGGER = logging.getLogger("YukkiMusic")
-
 async def main():
-      LOGGER.info("✅ All Directories Updated.")
       await bot.start() 
-
+      await app.start()
+      await call.start() 
+      await idle()
+    
 if __name__ == "__main__":
-    loop.run_until_complete(main())
+     print("Bot is starting...")
+     loop.run_until_complete(main())
