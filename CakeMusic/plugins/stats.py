40116b2d642fab476f44408001dbff76cfebaf0c
Teamonix <unioxo@outlook.com>
2024-12-06 04:27:30 +0530

    Update stats.py

--- CakeMusic/plugins/stats.py
+++ CakeMusic/plugins/stats.py
@@ -29,3 +29,33 @@ async def check_bot_stats(client, message):
         caption=caption,
         reply_markup=buttons,
     )
+
+
+@bot.on_callback_query(rgx("check_stats"))
+async def check_total_stats(client, query):
+    try:
+        user_id = query.from_user.id
+        runtime = __start_time__
+        boot_time = int(time.time() - runtime)
+        uptime = get_readable_time((boot_time))
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        activ_chats = len(ACTIVE_MEDIA_CHATS)
+        audio_chats = len(ACTIVE_AUDIO_CHATS)
+        video_chats = len(ACTIVE_VIDEO_CHATS)
+        
+        return await query.answer(
+            f"""⏱️ Bot Run Time [Boot]
+☛ {uptime}
+
+🔴 Served Chats: {served_chats}
+🔵 Served Users: {served_users}
+
+🦋 Total Active Chats [{activ_chats}]
+✿⋟ Audio Stream: {audio_chats}
+✿⋟ Video Stream: {video_chats}""",
+            show_alert=True
+        )
+    except Exception as e:
+        LOGGER.info(f"🚫 Stats Error: {e}")
+        pass
