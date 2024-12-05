803ea2efdbfcfced3f84dbc00b4752abe193261b
Teamonix <unioxo@outlook.com>
2024-12-06 00:41:47 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, sys
 from CakeMusic import bot
 from CakeMusic.plugins.play import app, call
 
@@ -7,7 +7,8 @@ async def main():
           await call.start()
           await bot.start() 
           await app.start() 
-
+      except Exception as e:
+          sys.exit()
 
 # Run the bot
 if __name__ == "__main__":
