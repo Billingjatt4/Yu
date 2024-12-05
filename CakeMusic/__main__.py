160bf41c6c8269f3b674ba23c467aa3d87acca3f
Teamonix <unioxo@outlook.com>
2024-12-06 01:45:53 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -5,7 +5,7 @@ from CakeMusic import bot, app, call
 loop = asyncio.get_event_loop()
 
 async def main():
-      plugins_path = Path(plugins).parent  # Get the plugins directory
+      plugins_path = CakeMusic/plugins  # Get the plugins directory
       plugin_files = [f.stem for f in plugins_path.glob("*.py") if f.is_file() and not f.stem.startswith("__")]
 
       for plugin in plugin_files:
