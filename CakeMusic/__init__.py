4fb0dbb73dbea2b92b33dfd78bf20d79214954ec
Teamonix <unioxo@outlook.com>
2024-12-05 22:34:20 +0530

    Update __init__.py

--- CakeMusic/__init__.py
+++ CakeMusic/__init__.py
@@ -1,6 +1,8 @@
 from typing import Union, List, Pattern
 from pyrogram import Client, filters as pyrofl
 from config import *
+import os
+import glob
 
 
 
@@ -10,6 +12,14 @@ app = Client(
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
 )
+
+
+# Dynamically load plugins
+PLUGIN_DIR = "CakeMusic/plugins"
+for plugin_file in glob.glob(f"{PLUGIN_DIR}/*.py"):
+    plugin_name = os.path.basename(plugin_file)[:-3]
+    if plugin_name != "__init__":
+        __import__(f"{PLUGIN_DIR}.{plugin_name}")
 
 def cdx(commands: Union[str, List[str]]):
     return pyrofl.command(commands, ["/", "!", "."])
