186d4be7106a541076f346ed12b67d3c452664da
Teamonix <unioxo@outlook.com>
2024-12-05 23:56:37 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -1,52 +1,12 @@
-import aiohttp, aiofiles, asyncio, base64, logging
-import os, platform, random, re, socket
-import sys, time, textwrap
+import os, logging
 from CakeMusic import app as bot
 from CakeMusic.plugins.Play import app, call
-from os import getenv
-from io import BytesIO
-from time import strftime
-from functools import partial
-from dotenv import load_dotenv
-from datetime import datetime
-from typing import Union, List, Pattern
 from logging.handlers import RotatingFileHandler
-import os
-import glob
-
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_async_
-
-from pyrogram import Client, filters as pyrofl
-from pytgcalls import PyTgCalls, filters as pytgfl
-
-
-from pyrogram import idle, __version__ as pyro_version
-from pytgcalls.__version__ import __version__ as pytgcalls_version
-
-from ntgcalls import TelegramServerError
-from pyrogram.enums import ChatMemberStatus, ChatType
-from pyrogram.errors import (
-    ChatAdminRequired,
-    FloodWait,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
-from pytgcalls.exceptions import NoActiveGroupCall
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pytgcalls.types import ChatUpdate, Update, GroupCallConfig
-from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
-
-from PIL import Image, ImageDraw, ImageEnhance
-from PIL import ImageFilter, ImageFont, ImageOps
-from youtubesearchpython.__future__ import VideosSearch
+from pyrogram import idle, Client, filters
 from config import *
 
 
 loop = asyncio.get_event_loop()
-
 
 logging.basicConfig(
     format="[%(name)s]:: %(message)s",
@@ -58,9 +18,7 @@ logging.basicConfig(
     ],
 )
 
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("asyncio").setLevel(logging.ERROR)
-logging.getLogger("httpx").setLevel(logging.ERROR)
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
@@ -124,11 +82,6 @@ async def main():
     except Exception as e:
         LOGGER.info(f"🚫 Assistant Error: {e}")
         sys.exit()
-    try:
-        await app.join_chat("AdityaServer")
-        await app.join_chat("AdityaDiscus")
-    except Exception:
-        pass
     if LOG_GROUP_ID != 0:
         try:
             await app.send_message(LOG_GROUP_ID, "**🦋 Assistant Started.**")
@@ -143,11 +96,7 @@ async def main():
     LOGGER.info("✅ PyTgCalls Started.")
     await asyncio.sleep(1)
     LOGGER.info("✅ Sucessfully Hosted Your Bot !!")
-    LOGGER.info("✅ Now Do Visit: @AdityaServer !!")
     await idle()
-
-
-
 
 
 if __name__ == "__main__":
