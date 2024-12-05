9a29227cf1a06d93f488777560a34439e44b8480
Teamonix <unioxo@outlook.com>
2024-12-06 00:33:54 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -1,17 +1,8 @@
-import logging
 from CakeMusic import bot
 from CakeMusic.plugins.play import app, call
-from logging.handlers import RotatingFileHandler
-from pyrogram import idle, Client, filters
-
-
-# Enable logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
 
 # Run the bot
 if __name__ == "__main__":
-    logger.info("Bot is starting...")
+    print("Bot is starting...")
     bot.run()
+    app.run() 
