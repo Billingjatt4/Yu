1c19bbb10946506eb9df2d0e46aba58fe1b4dc98
Teamonix <unioxo@outlook.com>
2024-12-06 02:04:26 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -3,18 +3,16 @@ from pyrogram import idle
 from CakeMusic import bot, app, call
 from CakeMusic.plugins import ALL_MODULES
 
-#loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
 
-def main():
+async def main():
       for all_module in ALL_MODULES:
-        importlib.import_module("CakeMusic.plugins" + all_module)
-      bot.start() 
-      app.start()
-      call.start() 
-      idle()
+      importlib.import_module("CakeMusic.plugins" + all_module)
+     await bot.start() 
+     await app.start()
+     await call.start() 
+     await idle()
     
 if __name__ == "__main__":
-   #  print("Bot is starting...")
-    # print("All plugins imported successfully.")
-    # loop.run_until_complete(main())
-     main()
+     print("Bot is starting...")
+     loop.run_until_complete(main())
