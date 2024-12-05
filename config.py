f9a8262a3b2ef53b0e5eaa04282dd2244af0b7da
Teamonix <unioxo@outlook.com>
2024-12-06 03:38:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
-
 from os import getenv
-from io import BytesIO
-from dotenv import load_dotenv
-# config variables
-if os.path.exists("Config.env"):
-    load_dotenv("Config.env")
 
 API_ID = int(getenv("API_ID", "25742938"))
 API_HASH = getenv("API_HASH", "b35b715fe8dc0a58e8048988286fc5b6")
